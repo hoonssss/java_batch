5780ae1ae6993c81b7c3003361ca72c8e940ab9d
yunjaehun <wogns8030@naver.com>
2024-09-24 22:13:19 +0900

    feat : java_batch ExecutionContext

---
+++ src/main/java/com/example/springbatch/ExecutionContextTasklet2.java
@@ -0,0 +1,31 @@
+package com.example.springbatch;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExecutionContextTasklet2 implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+
+        ExecutionContext jobExecutionContext = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
+        ExecutionContext stepExecutionContext = chunkContext.getStepContext().getStepExecution().getExecutionContext();
+
+        String jobName = chunkContext.getStepContext().getStepExecution().getJobExecution().getJobInstance().getJobName();
+        String stepName = chunkContext.getStepContext().getStepExecution().getStepName();
+
+        System.out.println("JobName : " + jobExecutionContext.get("jobName"));
+        System.out.println("StepName : " + stepExecutionContext.get("Step"));
+
+        if(stepExecutionContext.get("StepName") == null) {
+            stepExecutionContext.put("StepName", stepName);
+        }
+
+        return RepeatStatus.FINISHED;
+    }
+}
